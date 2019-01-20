2ad531672611a91f21769747f2014cade78b6cb8
Yago Rogatto Leme <yaguetto@Yagos-Macbook.local>
2019-01-20 12:52:58 -0200

    usando a framework FLASK e fazendo a condicional do metodo POST

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
-import socket
-import threading
+from flask import Flask
+app = Flask(__name__)
 
-def main():
-    HOST = ''                 # Symbolic name meaning all available interfaces
-    PORT = 50008              # Arbitrary non-privileged port
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((HOST, PORT))
-    s.listen(0)
-    while True:
-        conn, addr = s.accept()
-        print('Connected by', addr)
-        t = threading.Thread(target=handle_connection, args=(conn,))
-        t.start()
+index = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Brainfuck interpreter</title>
+    <h1>Brainfuck interpreter</h1>
+    <p>Interpretador de Brainfuck desenvolvido por Gustavo Kawamoto e Yago Rogatto.</p>
+</head>
+<body>
+    <form action="/" method="POST">
+        <div>
+            <label for="code">Código:</label>
+            <br>
+                <textarea name="code"></textarea>
+        </div>
+            <input type="submit">
+    </form>
 
-def handle_connection(conn):
-    buffer = b''
+    <dl>
+        <dt>GitHub Gustavo:</dt>
+        <dd><li><a href = "https://github.com/gkawamoto">Gkawamoto</a></li></dd>
+        <dt>GitHub Yago:</dt>
+        <dd><li><a href = "https://github.com/yaguetto">Yaguetto</a></li></d>
+    </dl>
+</body>
+</html>>
+'''
 
-    while 1:
-        data = conn.recv(4)
-        if not data: break
-        buffer = buffer + data
-        if b'\r\n\r\n' in buffer:
-            cabecalhos = buffer.split(b'\r\n')
-            cabecalho = cabecalhos[0]
-            buffer = b''
-            caminho = cabecalho.split(b' ')[1]
-            tamanho = 5 + len(caminho)
-            tamanho_string = str(tamanho)
-            tamanho_bytes = tamanho_string.encode()
-            conn.sendall(b'HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nContent-Length: '+tamanho_bytes+b'\r\n\r\nola, '+ caminho)
-        #conn.sendall(data)
-        #print(buffer)
-    conn.close()
+@app.route("/")
+def serve_index():
+    return index
 
-
-if __name__ == "__main__":
-    main()
+@app.route("/", methods=["POST"])
+def processa_brainfuck():
+    return 'x'
